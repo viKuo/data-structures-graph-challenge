f8f83b84bb168b3a67816a7b9a8254e5a868843b
viKuo <lost.angel.fin@gmail.com>
2016-08-11 21:17:40 -0500

    finish implementing graph

--- node.rb
+++ node.rb
@@ -1,3 +1,31 @@
 class Node
+	attr_accessor :value
+	def initialize(value)
+		@value = value
+		@edges = []
+	end
 
+	def add_edge(other_node)
+		@edges << other_node
+	end
+
+	def nodes
+		@edges
+	end
+
+	def exists?
+		nodes_to_search = @edges
+		if yield(self)
+			return true
+		end
+
+		until nodes_to_search.empty?
+			node = nodes_to_search.pop
+			if yield(node)
+				return true
+			end
+			nodes_to_search += node.nodes
+		end
+		false
+	end
 end
--- node_spec.rb
+++ node_spec.rb
@@ -1,5 +1,28 @@
 require_relative 'node'
 
 describe Node do
+	let!(:node) { Node.new("Matt") }
+	before(:each) do
+		mike = Node.new("Mike")
+		duke = Node.new("Duke")
+		maurice = Node.new("Maurice")
+		alyssa = Node.new("Alyssa")
+		casey = Node.new("Casey")
+		jones = Node.new("Jones")
+		node.add_edge(mike)
+		node.add_edge(maurice)
+		mike.add_edge(duke)
+		mike.add_edge(maurice)
+		mike.add_edge(casey)
+		duke.add_edge(alyssa)
+		jones.add_edge(casey)
+	end
 
+	it "can find Alyssa downstream from Matt" do
+		expect(node.exists?{ |node| node.value == "Alyssa" }).to eq true
+	end
+
+	it "cannot find Jones downstream from Matt" do
+		expect(node.exists?{ |node| node.value == "Jones" }).to eq false
+	end
 end
